cff6ce8b083742ee1b969f74e3d2207b47f4ef1b
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-08 00:45:58 -0500

    Add: Setup for server (prod)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(name='python-tester',
+      version='1.0.0',
+      description='',
+      long_description='',
+      url='',
+      license='MIT')
